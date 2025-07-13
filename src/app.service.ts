a88178aaabf6fcc9f88dc931b2fc8fffa4d4c822
SandraRodri <sandra14taty@gmail.com>
2025-07-13 10:31:30 -0500

    added allow cors

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Biblioteca';
+    return 'Biblioteca Backend API is running! 🚀 Version: 2025.07.13.10.31';
   }
 }
