b723eb8d4e30862d6ca6f83e464da0d7b82aad74
SandraRodri <sandra14taty@gmail.com>
2025-07-14 20:21:36 -0500

    Fix: port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000; // 👈 ¡esto es obligatorio en Render!
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 
   console.log(`Servidor escuchando en el puerto ${port}`);
