720add1bd2cceef44db8626df1aea85c5f4db797
SandraRodri <sandra14taty@gmail.com>
2025-07-15 11:33:02 -0500

    feat: set enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT || 3000;
+  app.enableCors();
   await app.listen(port);
 
   console.log(`Servidor escuchando en el puerto ${port}`);
